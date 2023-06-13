5137069a5380b7e75b3fac3d46ff75cdc027c71a
Brijesh Yadav <93579764+Brijesh-yadavv@users.noreply.github.com>
2023-06-13 23:31:07 +0530

    Create MiddleOfLinkedList.java

---
+++ Lisked List/MiddleOfLinkedList.java
@@ -0,0 +1,42 @@
+/****************************************************************
+
+ Following is the class structure of the Node class:
+
+ class Node {
+     public int data;
+     public Node next;
+
+     Node()
+     {
+         this.data = 0;
+         this.next = null;
+     }
+     Node(int data)
+     {
+         this.data = data;
+         this.next = null;
+     }
+     Node(int data, Node next)
+     {
+         this.data = data;
+         this.next = next;
+     }
+ }
+
+ *****************************************************************/
+
+public class Solution
+{
+    public static Node findMiddle(Node head)
+    {
+        Node s=head;
+        Node f=head;
+        while(f!=null && f.next!=null){
+            s=s.next;
+            f=f.next.next;
+
+        }
+        return s;
+
+    }
+}
